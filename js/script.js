834f1f29492bcdafd6c0c8368a02313a8896f612
lugaye <160602222+lugaye@users.noreply.github.com>
2025-08-25 11:28:55 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const birthYear = 2002; 
+
+console.log('I was born in ' + birthYear);
+
+//Arithmetic (Mathematical) Operators +,-, /, *
+console.log(10 +50);
+console.log(10 * 50);
+console.log(10 / 50);
+console.log(20 % 6); // modulus
+
+//comparison operators
+console.log(5 == "5"); //true (loose comparison - value )
+console.log(5 === "5"); //not true (strict - value and data type)
+
+// Functions named block of code
+/*
+Declaring functions
+
+1.function functionName(){
+    statements
+}
+
+
+*/
+
+function greet(){
+    console.log('Hello world. This message is through a function')
+}
+
+greet();
+
+// 2. function expression - store a function inside a variable
+
+const hello = function(){
+    console.log('Hi there! This is a message using a function expression.')
+}
+
+hello();
+
+// 3. arrow function
+
+const goodbye = () => console.log("Goodbye from an arrow function");
+
+goodbye();
+
+//parameters and return values
+
+function multiply(numberOne, numberTwo, numberThree){
+    return numberOne * numberTwo * numberThree;
+}
+
+console.log(multiply(2, 3, 10));
+console.log(multiply(6, 3, 10));
+console.log(multiply(5, 3, 10));
+console.log(multiply(9, 3, 10));
+console.log(multiply(9, 83, 10));
+
